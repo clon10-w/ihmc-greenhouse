6ed2da73746207af3d935145948c6c3068a0a448
clon10-w <150766143+clon10-w@users.noreply.github.com>
2023-12-30 09:56:57 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -29,35 +29,6 @@ namespace IHMCWarmhouse {
         return result
     }
 
-    /*//% subcategory="Upload"
-    //% weight=29
-    //% blockGap=8
-    //% blockId=ihmc_upload_thingspeak
-    //% block="Upload to ThingSpeak|Write API key %wAK|Field 1 %f1||Field 2 %f2|Field 3 %f3|Field 4 %f4|Field 5 %f5|Field 6 %f6|Field 7 %f7|Field 8 %f8"
-    export function altuploadThingspeak(   wAK: string,
-                                        f1: number,
-                                        f2: number = null,
-                                        f3: number = null,
-                                        f4: number = null,
-                                        f5: number = null,
-                                        f6: number = null,
-                                        f7: number = null,
-                                        f8: number = null  ) 
-    {
-        esp8266.uploadThingspeak(wAK,f1,f2,f3,f4,f5,f6,f7,f8)
-        for (let index = 0; index <= 5; index++) 
-        {
-            if (!(esp8266.isThingspeakUploaded())) 
-            {
-                esp8266.uploadThingspeak(wAK,f1,f2,f3,f4,f5,f6,f7,f8)
-                if (index == 5) 
-                {
-                    control.reset()
-                }
-            }
-        }
-    }*/
-
     //% subcategory="Upload"
     //% weight=29
     //% blockGap=8
